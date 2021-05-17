43f0bd843cc6b5c3d960fdc2cd4059601cb390d0
marcocanopoli <canopoli.marco@gmail.com>
2021-05-17 14:42:51 +0200

    Done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+*/
+
+var app = new Vue ({
+    el : '#root',
+    data : {
+        h1: 'Why are you wearing that stupid man suit?',
+        imgSrc: "https://www.ciakclub.it/wp-content/uploads/2020/04/Donnie-Darko-di-Richard-Kelly-e1571909010202-1600x866.jpg"
+    }
+    });
